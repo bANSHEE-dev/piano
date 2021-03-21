26f1abbf9c862c3f6b39bc528d678118878edc91
Andrey Sankovich <53842066+bANSHEE-dev@users.noreply.github.com>
2021-03-21 21:47:24 +0300

    Add files via upload

--- virtual-piano/js/script.js
+++ virtual-piano/js/script.js
@@ -1,27 +1,91 @@
 const piano = document.querySelector('.piano');
 const pianoКeys = document.querySelectorAll('.piano-key');
-
 const audio = document.querySelector('audio');
 const button = document.querySelector('button');
-
-const switchButtonOn = document.querySelector('.openfullscreen');
-const switchButtonOff = document.querySelector('.fullscreen');
-
-
-piano.addEventListener('click', (event) => playAudio(event));
-piano.addEventListener('click', playAudio);
+const fullScreenBtn = document.querySelector('.openfullscreen');
+const btnNotes = document.querySelector('.btn-notes');
+const btnLetters = document.querySelector('.btn-letters');
+const keys = document.querySelectorAll(".key");
 
 
-function playAudio() {
-  audio.currentTime = 0;
-  audio.play();
-}
-button.addEventListener('click', playAudio);
+// Переключение с букв на ноты. *****************************************
 
-
-window.addEventListener('keydown', (event) => {
-  if (event.code === 'KeyD' || event.code === 'KeyF' || event.code === 'KeyG' || event.code === 'KeyH' || event.code === 'KeyJ' || event.code === 'KeyK' || event.code === 'KeyL') {
-    playAudio();
-  }
+btnNotes.addEventListener('click', (btn) => {
+  btn.target.classList.add('btn-active');
+  btnLetters.classList.remove('btn-active');
+  pianoКeys.forEach(key => {
+    key.classList.remove('piano-list-letters')
+    key.classList.remove('letters')
+  })
 });
+
+btnLetters.addEventListener('click', (btn) => {
+  btn.target.classList.add('btn-active');
+  btnNotes.classList.remove('btn-active');
+  pianoКeys.forEach(key => {
+    key.classList.remove('piano-list-letters')
+    key.classList.add('letters')
+  })
+
+});
+//**********************************************************************
+
+//Действия при нажатии/зажатии мышки.
+const startSound = (event) => {
+  event.target.classList.add("active");
+};
+const stopSound = (event) => {
+  event.target.classList.remove("active");
+};
+
+const startCorrespondOver = (event) => {
+  event.target.classList.add("active");
+  pianoКeys.forEach((elem) => {
+    elem.addEventListener("mouseover", startSound)
+    elem.addEventListener("mouseout", stopSound)
+  });
+}
+
+const stopCorrespondOver = () => {
+  pianoКeys.forEach((elem) => {
+    elem.classList.remove("active");
+    elem.removeEventListener("mouseover", startSound)
+    elem.removeEventListener("mouseout", stopSound)
+  });
+}
+//******************************************************************************************************
+
+//Fullscreen method
+document.querySelector('.openfullscreen').addEventListener("click", toggleScreen);
+function toggleScreen() {
+  if(document.fullscreenElement === null) {
+    document.documentElement.requestFullscreen();
+  } else {
+    if(document.fullscreenEnabled) {
+      document.exitFullscreen();
+    }
+  }
+}
+//*******************************************************************************************************
+
+
+
+//Играем по нотам
+
+keys.forEach(pianoКeys => {
+  pianoКeys.addEventListener("click", () => playNote(pianoКeys))
+})
+
+function playNote (pianoКeys) {
+  const noteAudio = document.getElementById(pianoКeys.dataset.note)
+  noteAudio.currentTime = 0;
+  noteAudio.play(pianoКeys);
+
+}
+
+
+//************************************************************************
+piano.addEventListener("mousedown", startCorrespondOver, false);
+piano.addEventListener("mouseup", stopCorrespondOver);
+
 
